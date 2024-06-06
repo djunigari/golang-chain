bd23a708dabfb7569215f763ac0c68f99e484518
Alexandre Djun Igari <djun_igari@hotmail.com>
2024-06-06 11:22:47 +0900

    Add log error

--- errors.go
+++ errors.go
@@ -1,9 +1,30 @@
 package chain
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	logger "github.com/djunigari/golang-logger"
+)
 
 var (
 	ErrVariableNotFound    = errors.New("variable not found in context")
 	ErrInvalidVariableType = errors.New("invalid type of variable")
 	ErrInvalidVariable     = errors.New("invalid variable")
 )
+
+func LogError[T any](parentName string) *Action[T] {
+	return NewAction[T]("chains.LogError").
+		IgnoreError(true).
+		Function(func(ctx *Context[T]) {
+			if ctx.Err() != nil {
+				var errorDetails string
+				if ctx.ActionErr != nil {
+					errorDetails = fmt.Sprintf("[%s] %s : %s", ctx.ActionErr.Name, ctx.Err(), ctx.ErrMsg())
+				} else {
+					errorDetails = fmt.Sprintf("%s : %s", ctx.Err(), ctx.ErrMsg())
+				}
+				logger.LogError("failed "+parentName, errorDetails)
+			}
+		})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/djunigari/golang-chain
 
 go 1.22.1
+
+require github.com/djunigari/golang-logger v0.0.1
+
+require (
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
